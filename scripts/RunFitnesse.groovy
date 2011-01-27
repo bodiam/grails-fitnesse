364b1ce4b1b75b8f005c91f2664b4aae527b5b7b
Marcin Erdmann <erdi84@gmail.com>
2011-01-27 23:10:32 +0100

    Added RunFitnesse script that starts Fitnesse server and hangs; can be killed with ctrl+c

---
+++ scripts/RunFitnesse.groovy
@@ -0,0 +1,17 @@
+import fitnesse.Arguments
+import fitnesseMain.FitNesseMain
+import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
+
+includeTargets << grailsScript('_GrailsInit')
+includeTargets << grailsScript('_GrailsPackage')
+
+target('default': 'Starts Fitnesse Server') {
+    depends(checkVersion, compile, createConfig)
+    def fitnesseConfig = CH.config?.grails?.plugins?.fitnesse
+
+    ant.java(jar: "${fitnessePluginDir}/lib/fitnesse.jar", fork: true) {
+	arg(value: '-d')
+	arg(path: "${fitnesseConfig?.server?.dir ?: 'wiki'}")
+	arg(line: "-p ${fitnesseConfig?.server?.port ?: 9090}")
+    }
+}
