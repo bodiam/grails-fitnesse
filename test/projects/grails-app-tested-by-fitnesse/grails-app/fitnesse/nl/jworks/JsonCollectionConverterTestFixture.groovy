a8085edb9fa637f6182618eb3834bff28a1cbece
Marcin Erdmann <erdi84@gmail.com>
2011-03-15 20:37:54 +0100

    Fixed package issues in test for converting complex json objects and collections

--- test/projects/grails-app-tested-by-fitnesse/grails-app/fitnesse/nl/jworks/JsonCollectionConverterTestFixture.groovy
+++
@@ -1,9 +0,0 @@
-package nl.jworks
-
-import nl.jworks.domain.CarProducer
-
-class JsonCollectionConverterTestFixture {
-
-    CarProducer producer
-
-}
---
+++ test/projects/grails-app-tested-by-fitnesse/grails-app/fitnesse/nl/jworks/grails/fitnesse/feature/JsonCollectionConverterTestFixture.groovy
@@ -0,0 +1,7 @@
+package nl.jworks.grails.fitnesse.feature
+
+import nl.jworks.grails.fitnesse.feature.hibernate.CarProducer
+
+class JsonCollectionConverterTestFixture {
+    CarProducer producer
+}
