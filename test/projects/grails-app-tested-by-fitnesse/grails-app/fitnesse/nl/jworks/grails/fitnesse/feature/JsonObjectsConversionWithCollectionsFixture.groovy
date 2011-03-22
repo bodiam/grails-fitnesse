5fee86f6b41c9df447057dc6e14e6828842a5b5d
Marcin Erdmann <erdi84@gmail.com>
2011-03-22 20:38:54 +0100

    Renamed fixture testing json objects conversion for objects containing collections

--- test/projects/grails-app-tested-by-fitnesse/grails-app/fitnesse/nl/jworks/grails/fitnesse/feature/JsonObjectsConversionWithCollectionsFixture.groovy
+++ test/projects/grails-app-tested-by-fitnesse/grails-app/fitnesse/nl/jworks/grails/fitnesse/feature/JsonObjectsConversionWithCollectionsFixture.groovy
@@ -3,7 +3,7 @@ package nl.jworks.grails.fitnesse.feature
 import nl.jworks.grails.fitnesse.feature.hibernate.CarProducer
 import nl.jworks.grails.fitnesse.feature.hibernate.CarModel
 
-class JsonCollectionConverterTestFixture {
+class JsonObjectsConversionWithCollectionsFixture {
     CarProducer producer
 
     List<CarModel> models
