e692319494f34221b6a16f60bab5018c6e851766
Marcin Erdmann <erdi84@gmail.com>
2011-03-30 00:18:54 +0200

    Added configuration entry for GWZ steps base package, added another GWZ test table with and statement

--- grails-app/fitnesse/fitnesse/grails/GivWenZenForGrails.groovy
+++ grails-app/fitnesse/fitnesse/grails/GivWenZenForGrails.groovy
@@ -5,10 +5,15 @@ import nl.jworks.grails.plugin.fitnesse.Fixture
 import org.givwenzen.DomainStepFinder
 import org.givwenzen.GivWenZenExecutorCreator
 import org.givwenzen.GivWenZenForSlim
+import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
 
 @Fixture
 class GivWenZenForGrails extends GivWenZenForSlim {
 	GivWenZenForGrails() {
-		super(GivWenZenExecutorCreator.instance().domainStepFinder(new DomainStepFinder("nl.jworks.grails.fitnesse.gwz")).create()); // TODO configurable package name
+        super(GivWenZenExecutorCreator
+                .instance()
+                .stepClassBasePackage(CH.config.grails.plugin.fitnesse.givWenZen.packageName ?: "bdd.steps")
+                .create()
+        );
 	}
 }
--- test/projects/grails-app-tested-by-fitnesse/grails-app/conf/Config.groovy
+++ test/projects/grails-app-tested-by-fitnesse/grails-app/conf/Config.groovy
@@ -105,6 +105,9 @@ grails {
                 dir = 'wiki'
                 defaultSuite = ["FrontPage.GrailsTestSuite.SlimTestSystem?suite"]
             }
+            givWenZen {
+                packageName = 'nl.jworks.grails.fitnesse.gwz'
+            }
         }
     }
 }
