62dcecdd78ccb53f5aad03c88178e6d8a9064099
John Engelman <john.r.engelman@gmail.com>
2012-02-27 18:31:55 -0600

    Update project to latest fitnesse

---
+++ test/projects/amasun/test/unit/nl/jworks/amasun/controller/console/ConsoleControllerTests.groovy
@@ -0,0 +1,12 @@
+package nl.jworks.amasun.controller.console
+
+import grails.test.*
+import grails.test.mixin.TestFor
+
+@TestFor(ConsoleController)
+class ConsoleControllerTests {
+
+    void testSomething() {
+
+    }
+}
