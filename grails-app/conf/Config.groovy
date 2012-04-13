071f98e94ea0edc13140b9bf2e64d307e47530d3
Erik Pragt <erik.pragt@jworks.nl>
2012-04-13 13:41:56 +0200

    Migrated documentation to new structure using migrate-docs.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -2,7 +2,7 @@ grails.doc.authors = 'Erik Pragt (<a href="http://www.jworks.nl">jWorks.nl</a>),
 grails.doc.license = 'Apache License 2.0'
 grails.doc.title = 'Fitnesse Grails Plugin'
 grails.doc.logo = """<a href="http://www.fitnesse.org" target="_blank"><img src="../img/FitNesseLogo.gif" border="0"/></a>"""
-grails.doc.sponsorLogo = """<a href="http://www.jworks.nl" target="_blank"><img src="../img/jworks-logo.jpg" border="0"/></a>"""
+grails.doc.sponsorLogo = """<a href="http://www.jworks.nl" target="_blank"><img src="../img/jworks-logo.png" border="0"/></a>"""
 grails.doc.images = new File("src/docs/images")
 grails.views.default.codec="none" // none, html, base64
 grails.views.gsp.encoding="UTF-8"
